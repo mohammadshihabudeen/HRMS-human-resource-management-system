4c8206eaf68e3fe52a5ace774795289b0027c135
Mohammad shihabudeen <city.shihabudeen@gmail.com>
2024-05-08 14:44:16 +0530

    done

---
+++ src/components/pim/Employee List/TableButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TableButton = () => {
+  return (
+    <>
+    <div className="button-group">
+    <button type="submit" className="primary-btn ">Add</button>
+    <button type="submit" className="btn btn-secondary">Delete</button>
+    </div>
+    </>
+  )
+}
+
+export default TableButton
